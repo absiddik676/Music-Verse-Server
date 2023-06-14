ae0856431581dab570183fc8c9e64fd21947b838
absiddik676 <absiddik8200@gmail.com>
2023-06-14 21:41:12 +0600

    jwt veryfy added

--- index.js
+++ index.js
@@ -10,6 +10,21 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' })
+      }
+      req.decoded = decoded;
+      next();
+    })
+  }
+  
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.l9yjteg.mongodb.net/?retryWrites=true&w=majority`;
@@ -51,19 +66,24 @@ async function run() {
             res.send(result)
         })
 
-        app.get('/bes-classes',async(req,res)=>{
+        app.get('/best-classes',async(req,res)=>{
             const result = await classesCollation.find().sort({ enrolledStudentNumber: -1 }).limit(6).toArray();
+            res.send(result)
+        })
+        app.get('/best-instructor',async(req,res)=>{
+            const query = {role:'instructor'}
+            const result = await usersCollation.find(query).limit(6).toArray();
             res.send(result)
         })
 
         // instructor class related api
-        app.post('/classes', async (req, res) => {
+        app.post('/classes',verifyJWT, async (req, res) => {
             const data = req.body;
             const result = await classesCollation.insertOne(data)
             res.send(result)
         })
 
-        app.get('/my-classes/:email', async (req, res) => {
+        app.get('/my-classes/:email',verifyJWT, async (req, res) => {
             const email = req.params.email;
             const query = { InstructorEmail: email };
             const result = await classesCollation.find(query).toArray();
@@ -141,7 +161,7 @@ async function run() {
             res.send(result)
         })
 
-        app.get('/selected-class/:email', async (req, res) => {
+        app.get('/selected-class/:email',verifyJWT, async (req, res) => {
             const email = req.params.email;
             const query = { studentEmail: email };
             const result = await selectedClassesCollation.find(query).toArray();
@@ -170,20 +190,20 @@ async function run() {
             res.send(result)
         })
 
-        app.get('/payment-history/:email', async (req, res) => {
+        app.get('/payment-history/:email',verifyJWT, async (req, res) => {
             const email = req.params.email;
             const query = { email: email };
             const result = await paymentHistoryCollation.find(query).toArray();
             res.send(result)
         })
 
-        app.post('/enrolled-data', async (req, res) => {
+        app.post('/enrolled-data',verifyJWT, async (req, res) => {
             const data = req.body;
             const result = await enrolledCollation.insertOne(data);
             res.send(result)
         })
 
-        app.get('/enrolled-data/:email', async (req, res) => {
+        app.get('/enrolled-data/:email',verifyJWT, async (req, res) => {
             const email = req.params.email;
             const query = { studentEmail: email }
             const result = await enrolledCollation.find(query).toArray();
@@ -191,7 +211,7 @@ async function run() {
         })
 
         // payment related api
-        app.post('/create-payment-intent', async (req, res) => {
+        app.post('/create-payment-intent',verifyJWT, async (req, res) => {
             const { price } = req.body;
             const amount = price * 100;
             const paymentIntent = await stripe.paymentIntents.create({
@@ -204,7 +224,7 @@ async function run() {
             });
         })
 
-        app.post('/payment-history', async (req, res) => {
+        app.post('/payment-history',verifyJWT, async (req, res) => {
             const data = req.body;
             const result = await paymentHistoryCollation.insertOne(data);
             res.send(result)
@@ -229,7 +249,7 @@ async function run() {
 
         // admin related api
 
-        app.patch('/make-instructor/:id', async (req, res) => {
+        app.patch('/make-instructor/:id',verifyJWT, async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
             const updateDoc = {
@@ -249,7 +269,7 @@ async function run() {
             res.send(result)
         })
 
-        app.patch('/make-admin/:id', async (req, res) => {
+        app.patch('/make-admin/:id',verifyJWT, async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
             const updateDoc = {
@@ -261,7 +281,7 @@ async function run() {
             res.send(result)
         })
 
-        app.patch('/approve-class/:id', async (req, res) => {
+        app.patch('/approve-class/:id',verifyJWT, async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
             const updateDoc = {
@@ -272,7 +292,7 @@ async function run() {
             const result = await classesCollation.updateOne(filter, updateDoc);
             res.send({ result, message: 'Class approved' })
         })
-        app.patch('/denied-class/:id', async (req, res) => {
+        app.patch('/denied-class/:id',verifyJWT, async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
             const updateDoc = {
@@ -282,6 +302,22 @@ async function run() {
             };
             const result = await classesCollation.updateOne(filter, updateDoc);
             res.send({ result, message: 'Class denied' })
+        })
+
+        app.put('/feedback/:id',verifyJWT,async(req,res)=>{
+            const id =  req.params.id;
+            const data = req.body;
+            console.log(id,data.data);
+            const filter = {_id:new ObjectId(id)};
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                  feedback: `${data.data}`
+                },
+              };
+              const result = await classesCollation.updateOne(filter, updateDoc, options);
+            console.log(result);
+            res.send(result)
         })
 
 
